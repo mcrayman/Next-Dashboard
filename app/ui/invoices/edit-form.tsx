1b5326b87f33b7dc1d519239b6f25c4b3dd7da69
Matt McMeans <mrmcmans@memphis.edu>
2025-01-25 19:23:58 -0600

    Feat form validation

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { updateInvoice } from '@/app/lib/actions';
+
 import { CustomerField, InvoiceForm } from '@/app/lib/definitions';
 import {
   CheckIcon,
@@ -10,15 +10,21 @@ import {
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
 
+import { updateInvoice, State } from '@/app/lib/actions';
+import { useActionState } from 'react';
+ 
 export default function EditInvoiceForm({
   invoice,
   customers,
 }: {
   invoice: InvoiceForm;
   customers: CustomerField[];
-}) {const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
+}) {
+  const initialState: State = { message: null, errors: {} };
+  const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
+  const [state, formAction] = useActionState(updateInvoiceWithId, initialState);
  
-  return <form action={updateInvoiceWithId}>
+  return <form action={formAction}>
 
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
